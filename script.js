c031f6ed6dee28a15cfe5097b90148cd2e9630ca
melissavelasquezgreene <melissa0191@outlook.es>
2023-11-08 13:58:58 -0500

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Benefits of Bootcamps to Develop Technical Skills";
+h2.textContent = "Benefits of Bootcamps to Develop Technical Skills.";
 
 document.querySelector("body").appendChild(h2);
+
+const ul = document.createElement("ul");
+const listItems = [
+  "ADOPT A NEW ROLE",
+  "A CHANCE TO ENTER A NEW CAREER",
+  "A CHANCE TO BUILD YOUR NETWORK"
+];
+
+listItems.forEach(item => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  ul.appendChild(li);
+});
+
+document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(ul);
